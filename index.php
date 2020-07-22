824e745ce90f6001282cd65fb77e84a685d199a0
Hamzah Waqas <ArkeologeN@users.noreply.github.com>
2020-07-22 19:43:21 +0200

    Added necessary API methods

--- index.php
+++ index.php
@@ -32,6 +32,27 @@ class Nonfig {
         throw new Exception("Unable to find a Configuration with id(" . $id . ")");
     }
     
+    function findConfigurationByName($name) {
+        $response = $this->executeRequest("configurations/name/" . $name);
+        
+        $this->handleError($response);
+        $configurations = $response->body->data;
+        
+        if (count($configurations) > 0) {
+            return $configurations[0];
+        }
+        
+        throw new Exception("Unable to find a Configuration with name(" . $id . ")");
+    }
+    
+    function findConfigurationByPath($path) {
+        $response = $this->executeRequest("configurations/path/" . $path);
+        
+        $this->handleError($response);
+        
+        return $response->body->data;
+    }
+    
     function executeRequest($path) {
         return Httpful\Request::get($this->apiUrl . "/" . $path)
             ->addHeader("Authorization", "Bearer " . $this->appId . ":" . $this->appSecret)
@@ -40,6 +61,8 @@ class Nonfig {
     
     function handleError($response) {
         if ($response->body->success == false) {
+            print_r($response);
+            
             throw new Exception("Failed to fetch configuration");
         }
     }
@@ -47,8 +70,10 @@ class Nonfig {
 
 // pls remove this below
 $nonfig = new Nonfig("f1afd7c6-c570-4fc0-a08a-5ed65bf9811e", "cT99KHl4kT17ewFotCef");
-$config = $nonfig->findConfigurationById("8400a3a0-9c14-47cc-b598-f5037fd5a9ce");
+$configId = $nonfig->findConfigurationById("8400a3a0-9c14-47cc-b598-f5037fd5a9ce");
+$configName = $nonfig->findConfigurationByName("/feature_flags");
+$configPath = $nonfig->findConfigurationByPath("i18n");
 
-print_r($config);
+print_r($configPath);
 
 ?>
