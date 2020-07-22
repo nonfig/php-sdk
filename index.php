dedbe46f848c1531729488b5528b77b079f0c40c
Hamza Waqas <ArkeologeN@users.noreply.github.com>
2020-07-22 20:08:45 +0200

    feat: added necessary methods and structure

--- index.php
+++ index.php
@@ -1,68 +1,67 @@
 <?php
 
-// includes Httpful library
-require "util/httpful.phar";
-
-// $url = "http://search.twitter.com/search.json";
-// $query = urlencode("#PHP");
-// $response = Httpful\Request::get($url . "?q=$query")->send();
-
-// echo "<pre>";
-// print_r($response);
+require 'vendor/autoload.php';
 
 class Nonfig {
-    
     function __construct($appId, $appSecret) {
         $this->appId = $appId;
         $this->appSecret = $appSecret;
         $this->baseUrl = "https://app.nonfig.com";
         $this->apiUrl = $this->baseUrl . "/api/v1";
     }
-    
+
     function findConfigurationById($id) {
         $response = $this->executeRequest("configurations/id/" . $id);
-        
+
         $this->handleError($response);
+
         $configurations = $response->body->data;
-        
+
         if (count($configurations) > 0) {
             return $configurations[0];
         }
-        
-        throw new Exception("Unable to find a Configuration with id(" . $id . ")");
+
+        throw new Exception(
+            "Unable to find a Configuration with id(" . $id . ")"
+        );
     }
-    
+
     function findConfigurationByName($name) {
         $response = $this->executeRequest("configurations/name/" . $name);
-        
+
         $this->handleError($response);
         $configurations = $response->body->data;
-        
+
         if (count($configurations) > 0) {
             return $configurations[0];
         }
-        
-        throw new Exception("Unable to find a Configuration with name(" . $id . ")");
+
+        throw new Exception(
+            "Unable to find a Configuration with name(" . $id . ")"
+        );
     }
-    
+
     function findConfigurationByPath($path) {
         $response = $this->executeRequest("configurations/path/" . $path);
-        
+
         $this->handleError($response);
-        
+
         return $response->body->data;
     }
-    
+
     function executeRequest($path) {
         return Httpful\Request::get($this->apiUrl . "/" . $path)
-            ->addHeader("Authorization", "Bearer " . $this->appId . ":" . $this->appSecret)
+            ->addHeader(
+                "Authorization",
+                "Bearer " . $this->appId . ":" . $this->appSecret
+            )
             ->send();
     }
-    
+
     function handleError($response) {
         if ($response->body->success == false) {
             print_r($response);
-            
+
             throw new Exception("Failed to fetch configuration");
         }
     }
